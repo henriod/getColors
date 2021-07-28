cf7b66f7f5250a310981349797c83f93908adfca
benard-home <odhiambb@gmail.com>
2021-07-28 09:46:16 +0300

    comments

--- getcolor.py
+++ getcolor.py
@@ -8,17 +8,26 @@ from colordetect import ColorDetect
 from heapq import nlargest, nsmallest
 
 def GetMinorColor(url):
+    #getting image content from the web using request
     img_data = requests.get(url).content
+    #creating a temp director to work on
     dir_name = tempfile.mkdtemp()
+    #create image file inside the temp directory
     dist_file = os.path.join(dir_name, 'img.jpg')
+    #opening and writing image content to the image file created
     f = open(dist_file, "wb")
     f.write(img_data)
     f.close()
+    #sending the the image to ColorDetect library
     img_detect = ColorDetect(dist_file)
+    #identifying colors present in the image which their percentages
     colors_dict = img_detect.get_color_count(color_format="hex")
+    #using nsmallest to get the color with smallest percentage in the dictionary
     minor_color = nsmallest(1, colors_dict, key=colors_dict.get)
+    #printing only color with percentage value
     print(minor_color[0])
     color = minor_color[0]
+    #cleaning up the temp 
     shutil.rmtree(dir_name)
     return color
 
